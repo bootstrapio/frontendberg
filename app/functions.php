e32219a19d9c12bde2ebf2d357cb55437372f22f
Joseph <josephcvan@gmail.com>
2019-01-25 23:15:17 -0500

    WDS Blocks v1.0.7 Added

--- app/functions.php
+++ app/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-/* -- Enqueue Scripts and Styles -- */
+// Theme | Enqueue Scripts + Styles
 add_action( 'wp_loaded', function() {
 	wp_register_script('vendor', get_theme_root_uri().'/.tmp/library/scripts/vendor.js', array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-components', 'wp-editor' ), '', true);
 	wp_enqueue_script('vendor');
@@ -11,3 +11,16 @@ add_action( 'wp_loaded', function() {
 	wp_register_style( 'main', get_theme_root_uri().'/.tmp/library/styles/main.css', array(), '' );
   wp_enqueue_style('main');
 });
+
+// Gutenberg | Block Categories
+add_filter( 'block_categories', function( $categories, $post ) {
+	return array_merge(
+		$categories,
+		array(
+			array(
+				'slug'  => 'frontendberg-blocks',
+				'title' => 'Blocks'
+			)
+		)
+	);
+}, 10, 2 );
