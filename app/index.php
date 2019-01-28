82e2f65803b9449bd7d716792c5d9acd49dcb4e3
Joseph <josephcvan@gmail.com>
2019-01-28 06:43:39 -0500

    iPhone Added

--- app/index.php
+++ app/index.php
@@ -6,7 +6,7 @@
 <section>
 	<div class="bd-example-row">
 		<div class="bd-example">
-			<div class="container layout-one">
+			<div class="container layout-two">
 				<div class="row block-content">
 					<div class="col superscript">
 						<figure class="media-icon ">
@@ -26,8 +26,8 @@
 				</div>
 				<div class="row block-media">
 					<div class="col">
-						<figure class="media-image device-ipad portrait">
-							<img alt="" class="img-fluid" src="https://www.placehold.it/556x742">
+						<figure class="media-image device-iphone portrait">
+							<img alt="" class="img-fluid" src="https://www.placehold.it/284x612">
 						</figure>
 					</div>
 				</div>
@@ -40,6 +40,32 @@
 <section>
 	<div class="bd-example-row">
 		<div class="bd-example">
+			<div class="container layout-one">
+				<div class="row block-content">
+					<div class="col superscript">
+						<figure class="media-icon ">
+							<img alt="" class="img-fluid" src="http://local.frontendberg/wp-content/uploads/2019/01/icon_imovie_mac__el9krw4zjm82_large_2x.png">
+						</figure>
+						<h5>iMovie on MacBook Pro</h5>
+					</div>
+					<div class="col">
+						<h3>You have a great touch for making movies.</h3>
+					</div>
+					<div class="col">
+						<div>
+							<p>iMovie is even easier to use with the new MacBook Pro, featuring the revolutionary Touch Bar. The most useful commands automatically appear on the keyboard, right where you need them. And MacBook Pro easily powers through demanding 4K video projects so you can edit and export in record time.</p>
+							<a href="#" class="">Layout Two</a>
+						</div>
+					</div>
+				</div>
+				<div class="row block-media">
+					<div class="col">
+						<figure class="media-image device-iphone">
+							<img alt="" class="img-fluid" src="https://www.placehold.it/612x284">
+						</figure>
+					</div>
+				</div>
+			</div>
 			<div class="container layout-two">
 				<div class="row block-content">
 					<div class="col superscript">
@@ -60,8 +86,8 @@
 				</div>
 				<div class="row block-media">
 					<div class="col">
-						<figure class="media-image device-ipad">
-							<img alt="" class="img-fluid" src="https://www.placehold.it/742x556">
+						<figure class="media-image device-iphone portrait">
+							<img alt="" class="img-fluid" src="https://www.placehold.it/284x612">
 						</figure>
 					</div>
 				</div>
@@ -94,8 +120,8 @@
 				</div>
 				<div class="row block-media">
 					<div class="col">
-						<figure class="media-image device-ipad">
-							<img alt="" class="img-fluid" src="https://www.placehold.it/742x556">
+						<figure class="media-image device-iphone">
+							<img alt="" class="img-fluid" src="https://www.placehold.it/612x284">
 						</figure>
 					</div>
 				</div>
@@ -120,8 +146,8 @@
 				</div>
 				<div class="row block-media">
 					<div class="col">
-						<figure class="media-image device-ipad portrait">
-							<img alt="" class="img-fluid" src="https://www.placehold.it/556x742">
+						<figure class="media-image device-iphone">
+							<img alt="" class="img-fluid" src="https://www.placehold.it/612x284">
 						</figure>
 					</div>
 				</div>
