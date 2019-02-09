30fa59255e83448bbb24ef2a27c913f6874b9649
Joseph <josephcvan@gmail.com>
2019-02-08 23:38:50 -0500

    Banner Block Created

--- app/blocks.php
+++ app/blocks.php
@@ -1,50 +1,6 @@
 <?php get_header(); ?>
 
 <main>
-<section>
-	<div class="bd-example-row">
-		<div class="bd-example">
-			<div class="container banner-group">
-				<div class="row">
-					<div class="col">
-						<div class="row group-content">
-							<div class="col superscript">
-								<figure class="media-icon ">
-									<img alt="" class="img-fluid" src="http://local.frontendberg/wp-content/uploads/2019/01/icon_imovie_mac__el9krw4zjm82_large_2x.png">
-								</figure>
-								<h5>iMovie on MacBook Pro</h5>
-							</div>
-							<div class="col">
-								<div>
-									<h3>iMovie on MacBook Pro</h3>
-									<p>iMovie is even easier to use with the new MacBook Pro, featuring the revolutionary Touch Bar. The most useful commands automatically appear on the keyboard, right where you need them. And MacBook Pro easily powers through demanding 4K video projects so you can edit and export in record time.</p>
-									<a href="#" class="">Block One</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col">
-						<div class="row group-content">
-							<div class="col superscript">
-								<figure class="media-icon ">
-									<img alt="" class="img-fluid" src="http://local.frontendberg/wp-content/uploads/2019/01/icon_imovie_mac__el9krw4zjm82_large_2x.png">
-								</figure>
-								<h5>iMovie on MacBook Pro</h5>
-							</div>
-							<div class="col">
-								<div>
-									<h3>iMovie on MacBook Pro</h3>
-									<p>iMovie is even easier to use with the new MacBook Pro, featuring the revolutionary Touch Bar. The most useful commands automatically appear on the keyboard, right where you need them. And MacBook Pro easily powers through demanding 4K video projects so you can edit and export in record time.</p>
-									<a href="#" class="">Block One</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
 
 <section>
 	<div class="bd-example-row">
@@ -62,62 +18,71 @@
 					</div>
 				</div>
 			</div>
-			<div class="container layout-one">
+			<div class="container block-group">
+			<div class="row group-media">
+				<div class="col">
+					<figure class="media-image device-iphone landscape placeholder">
+						<img alt="" class="img-fluid" src="https://www.placehold.it/490x660">
+					</figure>
+				</div>
+				<div class="col">
+					<figure class="media-image device-iphone portrait placeholder">
+						<img alt="" class="img-fluid" src="https://www.placehold.it/490x660">
+					</figure>
+				</div>
+			</div>
 				<div class="row group-content">
-					<div class="col superscript">
-						<figure class="media-icon ">
-							<img alt="" class="img-fluid" src="http://local.frontendberg/wp-content/uploads/2019/01/icon_imovie_mac__el9krw4zjm82_large_2x.png">
-						</figure>
-						<h5>iMovie on MacBook Pro</h5>
-					</div>
 					<div class="col">
-						<h3>You have a great touch for making movies.</h3>
+						<div>
+							<h6>iMovie on MacBook Pro</h6>
+							<p>iMovie is even easier to use with the new MacBook Pro, featuring the revolutionary Touch Bar. The most useful commands automatically appear on the keyboard, right where you need them. And MacBook Pro easily powers through demanding 4K video projects so you can edit and export in record time.</p>
+							<a href="#" class="">Block One</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="container block-group">
+				<div class="row group-content">
+					<div class="col">
+						<div>
+							<h6>iMovie on MacBook Pro</h6>
+							<p>iMovie is even easier to use with the new MacBook Pro, featuring the revolutionary Touch Bar. The most useful commands automatically appear on the keyboard, right where you need them. And MacBook Pro easily powers through demanding 4K video projects so you can edit and export in record time.</p>
+							<a href="#" class="">Layout One</a>
+						</div>
 					</div>
 					<div class="col">
 						<div>
+							<h6>iMovie on MacBook Pro</h6>
 							<p>iMovie is even easier to use with the new MacBook Pro, featuring the revolutionary Touch Bar. The most useful commands automatically appear on the keyboard, right where you need them. And MacBook Pro easily powers through demanding 4K video projects so you can edit and export in record time.</p>
 							<a href="#" class="">Layout One</a>
 						</div>
 					</div>
 				</div>
-				<div class="row group-media">
-					<div class="col">
-						<figure class="media-image device-ipad portrait placeholder">
-							<img alt="" class="img-fluid" src="https://www.placehold.it/490x660">
-						</figure>
-					</div>
-				</div>
 			</div>
-		</div>
-	</div>
-</section>
 
-<section>
-	<div class="bd-example-row">
-		<div class="bd-example">
-			<div class="container layout-two">
+			<div class="container block-group">
 				<div class="row group-content">
-					<div class="col superscript">
-						<figure class="media-icon ">
-							<img alt="" class="img-fluid" src="http://local.frontendberg/wp-content/uploads/2019/01/icon_imovie_mac__el9krw4zjm82_large_2x.png">
-						</figure>
-						<h5>iMovie on MacBook Pro</h5>
-					</div>
 					<div class="col">
-						<h3>You have a great touch for making movies.</h3>
+						<div>
+							<h6>iMovie on MacBook Pro</h6>
+							<p>iMovie is even easier to use with the new MacBook Pro, featuring the revolutionary Touch Bar. The most useful commands automatically appear on the keyboard, right where you need them. And MacBook Pro easily powers through demanding 4K video projects so you can edit and export in record time.</p>
+							<a href="#" class="">Layout One</a>
+						</div>
 					</div>
 					<div class="col">
 						<div>
+							<h6>iMovie on MacBook Pro</h6>
 							<p>iMovie is even easier to use with the new MacBook Pro, featuring the revolutionary Touch Bar. The most useful commands automatically appear on the keyboard, right where you need them. And MacBook Pro easily powers through demanding 4K video projects so you can edit and export in record time.</p>
-							<a href="#" class="">Layout Two</a>
+							<a href="#" class="">Layout One</a>
 						</div>
 					</div>
-				</div>
-				<div class="row group-media">
 					<div class="col">
-						<figure class="media-image device-ipad landscape">
-							<img alt="" class="img-fluid" src="https://www.placehold.it/660x490">
-						</figure>
+						<div>
+							<h6>iMovie on MacBook Pro</h6>
+							<p>iMovie is even easier to use with the new MacBook Pro, featuring the revolutionary Touch Bar. The most useful commands automatically appear on the keyboard, right where you need them. And MacBook Pro easily powers through demanding 4K video projects so you can edit and export in record time.</p>
+							<a href="#" class="">Layout One</a>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -148,7 +113,7 @@
 				</div>
 				<div class="row group-media">
 					<div class="col">
-						<figure class="media-image device-ipad landscape">
+						<figure class="media-image device-iphone landscape">
 							<img alt="" class="img-fluid" src="https://www.placehold.it/660x490">
 						</figure>
 					</div>
@@ -174,7 +139,7 @@
 				</div>
 				<div class="row group-media">
 					<div class="col">
-						<figure class="media-image device-ipad portrait">
+						<figure class="media-image device-iphone portrait">
 							<img alt="" class="img-fluid" src="https://www.placehold.it/490x660">
 						</figure>
 					</div>
