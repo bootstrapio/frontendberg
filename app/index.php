5ceee8267ec26eed37acbc1af67ba29ece6e0466
Joseph <josephcvan@gmail.com>
2019-02-10 00:36:16 -0500

    Banner Preview

--- app/index.php
+++ app/index.php
@@ -108,36 +108,36 @@
 					</div>
 				</div>
 			</div>
-				<div class="container banner-preview">
-					<div class="row">
-						<div class="col">
-							<div class="row group-content">
-								<div class="col superscript">
-									<figure class="media-icon ">
-										<img alt="" class="img-fluid" src="http://local.frontendberg/wp-content/uploads/2019/01/icon_imovie_mac__el9krw4zjm82_large_2x.png">
-									</figure>
-									<h5>iMovie on MacBook Pro</h5>
-								</div>
-								<div class="col">
-									<div>
-										<h3>iMovie on MacBook Pro</h3>
-										<p>iMovie is even easier to use with the new MacBook Pro, featuring the revolutionary Touch Bar. The most useful commands automatically appear on the keyboard, right where you need them. And MacBook Pro easily powers through demanding 4K video projects so you can edit and export in record time.</p>
-										<a href="#" class="">Block One</a>
-									</div>
-								</div>
+			<div class="container banner-preview">
+				<div class="row">
+					<div class="col">
+						<div class="row group-content">
+							<div class="col superscript">
+								<figure class="media-icon ">
+									<img alt="" class="img-fluid" src="http://local.frontendberg/wp-content/uploads/2019/01/icon_imovie_mac__el9krw4zjm82_large_2x.png">
+								</figure>
+								<h5>iMovie on MacBook Pro</h5>
 							</div>
-						</div>
-						<div class="col">
-							<div class="row group-media">
-								<div class="col">
-									<figure class="media-image device-ipad portrait">
-										<img alt="" class="img-fluid" src="https://www.placehold.it/490x660">
-									</figure>
+							<div class="col">
+								<div>
+									<h3>iMovie on MacBook Pro</h3>
+									<p>iMovie is even easier to use with the new MacBook Pro, featuring the revolutionary Touch Bar. The most useful commands automatically appear on the keyboard, right where you need them. And MacBook Pro easily powers through demanding 4K video projects so you can edit and export in record time.</p>
+									<a href="#" class="">Block One</a>
 								</div>
 							</div>
 						</div>
 					</div>
+					<div class="col">
+						<div class="row group-media">
+							<div class="col">
+								<figure class="media-image device-ipad portrait">
+									<img alt="" class="img-fluid" src="https://www.placehold.it/490x660">
+								</figure>
+							</div>
+						</div>
+					</div>
 				</div>
+			</div>
 		</div>
 	</div>
 </section>
