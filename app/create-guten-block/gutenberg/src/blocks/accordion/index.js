dbaf5e1eb298157c925fc10d9f7b5e631a5b982a
Joseph <josephcvan@gmail.com>
2019-03-17 20:11:57 -0400

    CGB Accordion Created

---
+++ app/create-guten-block/gutenberg/src/blocks/accordion/index.js
@@ -0,0 +1,87 @@
+/** BLOCK: Layout **/
+
+const { __ } = wp.i18n;
+const { registerBlockType } = wp.blocks;
+const { InspectorControls } = wp.editor;
+
+import classnames from 'classnames';
+import LayoutEditor, { LayoutStyleClasses } from '../../components/layout/';
+import LayoutInspector, { LayoutInspectorAttributes } from '../../components/layout/inspector';
+import SuperscriptEditor, { SuperscriptAttributes, SuperscriptFrontend } from '../../components/superscript';
+import SuperscriptInspector, { SuperscriptInspectorAttributes } from '../../components/superscript/inspector';
+import ContentEditor, { ContentAttributes, ContentFrontend } from '../../components/content';
+import ContentInspector, { ContentInspectorAttributes } from '../../components/content/inspector';
+import MediaEditor, { MediaAttributes, MediaFrontend } from '../../components/media';
+import MediaInspector, { MediaInspectorAttributes } from '../../components/media/inspector';
+
+export default registerBlockType( 'frontendberg/layout', {
+	category: 'frontendberg-layouts',
+	description: '',
+	icon: 'sos',
+	keywords: [ 'Options' ],
+	title: 'Layout',
+	attributes: {
+		...SuperscriptAttributes, ...SuperscriptInspectorAttributes,
+		...ContentAttributes, ...ContentInspectorAttributes,
+		...MediaAttributes, ...MediaInspectorAttributes,
+		...LayoutInspectorAttributes,
+	},
+
+	edit: props => {
+		return [
+			!! props.isSelected && (
+				<InspectorControls key="inspector">
+					<LayoutInspector { ...props } />
+					<SuperscriptInspector { ...props } />
+					<ContentInspector { ...props } />
+					<MediaInspector { ...props } />
+				</InspectorControls>
+			),
+			<section>
+				<div
+					className={ 'container ' + classnames(
+						props.className,
+						...LayoutStyleClasses( props ),
+					)}
+				>
+					<div class="row group-content">
+						<SuperscriptEditor { ...props } />
+						<ContentEditor { ...props } />
+					</div>
+					{( props.attributes.displayMedia === false ? '' :
+					<div class="row group-media">
+						<div class="col">
+							<MediaEditor { ...props } />
+						</div>
+					</div>
+					)}
+				</div>
+			</section>,
+		];
+	},
+
+	save: props => {
+		return (
+			<section>
+				<div
+					className={ 'container ' + classnames(
+						props.className,
+						...LayoutStyleClasses( props ),
+					)}
+				>
+					<div class="row group-content">
+						<SuperscriptFrontend { ...props } />
+						<ContentFrontend { ...props } />
+					</div>
+					{( props.attributes.displayMedia === false ? '' :
+					<div class="row group-media">
+						<div class="col">
+							<MediaFrontend { ...props } />
+						</div>
+					</div>
+					)}
+				</div>
+			</section>
+		);
+	}
+});
