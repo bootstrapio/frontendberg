4795ee47d9297f1d02b04765fb744e28ba9bfbe7
Joseph <josephcvan@gmail.com>
2019-03-27 01:31:03 -0400

    SASS Intro Files Updated

--- app/index.php
+++ app/index.php
@@ -1,9 +1,8 @@
 <?php get_header(); ?>
 
 <main>
-
 	<section>
-		<div class="container banner-one">
+		<div class="container intro-one">
 			<div class="row group-content">
 				<div class="col superscript">
 					<figure class="media-icon placeholder">
@@ -12,76 +11,16 @@
 					<h5>Deserunt Mollit</h5>
 				</div>
 				<div class="col">
-					<h3>Anim Id Est Laborum</h3>
-				</div>
-				<div class="col">
-						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam.</p>
-						<a href="#" class="">Ut enim ad minim</a>
-				</div>
-			</div>
-			<div class="row group-media">
-				<div class="col">
-					<figure class="media-image device-macbook placeholder">
-						<img alt="" class="img-fluid" src="">
-					</figure>
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<section>
-		<div class="container banner-two">
-			<div class="row group-content">
-				<div class="col superscript">
-					<figure class="media-icon placeholder">
-						<img alt="" class="img-fluid" src="">
-					</figure>
-					<h5>Deserunt Mollit</h5>
-				</div>
-				<div class="col">
-					<h3>Anim Id Est Laborum</h3>
+					<h2>Culpa Qui Oficia</h2>
 				</div>
 				<div class="col">
 					<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam.</p>
 					<a href="#" class="">Ut enim ad minim</a>
 				</div>
 			</div>
-			<div class="row group-media">
-				<div class="col">
-					<figure class="media-image device-macbook placeholder">
-						<img alt="" class="img-fluid" src="">
-					</figure>
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<section>
-		<div class="container layout-three">
-			<div class="row group-content">
-				<div class="col superscript">
-					<figure class="media-icon placeholder">
-						<img alt="" class="img-fluid" src="">
-					</figure>
-					<h5>Deserunt Mollit</h5>
-				</div>
-				<div class="col">
-					<h3>Anim Id Est Laborum</h3>
-				</div>
-				<div class="col">
-						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam.</p>
-						<a href="#" class="">Ut enim ad minim</a>
-				</div>
-			</div>
-			<div class="row group-media">
-				<div class="col">
-					<figure class="media-image device-macbook placeholder">
-						<img alt="" class="img-fluid" src="">
-					</figure>
-				</div>
-			</div>
 		</div>
 	</section>
 </main>
 
-<?php get_footer();
+<?php
+get_footer();
--- app/intros.php
+++ app/intros.php
@@ -1,145 +1,25 @@
 <?php get_header(); ?>
 
 <main>
-<section>
-	<div class="bd-example-row">
-		<div class="bd-example">
-			<div class="container intro-one">
-				<div class="row group-content">
-					<div class="col">
-						<h2>You have a great touch for making movies.</h2>
-					</div>
-					<div class="col">
-						<div>
-							<p>iMovie is even easier to use with the new MacBook Pro, featuring the revolutionary Touch Bar. The most useful commands automatically appear on the keyboard, right where you need them. And MacBook Pro easily powers through demanding 4K video projects so you can edit and export in record time.</p>
-							<a href="#" class="">Intro One</a>
-						</div>
-					</div>
+	<section>
+		<div class="container intro-one">
+			<div class="row group-content">
+				<div class="col superscript">
+					<figure class="media-icon placeholder">
+						<img alt="" class="img-fluid" src="">
+					</figure>
+					<h5>Deserunt Mollit</h5>
 				</div>
-			</div>
-			<div class="container layout-one">
-				<div class="row group-content">
-					<div class="col superscript">
-						<figure class="media-icon ">
-							<img alt="" class="img-fluid" src="http://local.frontendberg/wp-content/uploads/2019/01/icon_imovie_mac__el9krw4zjm82_large_2x.png">
-						</figure>
-						<h5>iMovie on MacBook Pro</h5>
-					</div>
-					<div class="col">
-						<h3>You have a great touch for making movies.</h3>
-					</div>
-					<div class="col">
-						<div>
-							<p>iMovie is even easier to use with the new MacBook Pro, featuring the revolutionary Touch Bar. The most useful commands automatically appear on the keyboard, right where you need them. And MacBook Pro easily powers through demanding 4K video projects so you can edit and export in record time.</p>
-							<a href="#" class="">Layout One</a>
-						</div>
-					</div>
+				<div class="col">
+					<h2>Culpa Qui Oficia</h2>
 				</div>
-				<div class="row group-media">
-					<div class="col">
-						<figure class="media-image device-ipad portrait placeholder">
-							<img alt="" class="img-fluid" src="https://www.placehold.it/490x660">
-						</figure>
-					</div>
+				<div class="col">
+					<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam.</p>
+					<a href="#" class="">Ut enim ad minim</a>
 				</div>
 			</div>
 		</div>
-	</div>
-</section>
-
-
-<section>
-	<div class="bd-example-row">
-		<div class="bd-example">
-			<div class="container layout-two">
-				<div class="row group-content">
-					<div class="col superscript">
-						<figure class="media-icon ">
-							<img alt="" class="img-fluid" src="http://local.frontendberg/wp-content/uploads/2019/01/icon_imovie_mac__el9krw4zjm82_large_2x.png">
-						</figure>
-						<h5>iMovie on MacBook Pro</h5>
-					</div>
-					<div class="col">
-						<h3>You have a great touch for making movies.</h3>
-					</div>
-					<div class="col">
-						<div>
-							<p>iMovie is even easier to use with the new MacBook Pro, featuring the revolutionary Touch Bar. The most useful commands automatically appear on the keyboard, right where you need them. And MacBook Pro easily powers through demanding 4K video projects so you can edit and export in record time.</p>
-							<a href="#" class="">Layout Two</a>
-						</div>
-					</div>
-				</div>
-				<div class="row group-media">
-					<div class="col">
-						<figure class="media-image device-ipad portrait placeholder">
-							<img alt="" class="img-fluid" src="https://www.placehold.it/660x490">
-						</figure>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-
-
-<section>
-	<div class="bd-example-row">
-		<div class="bd-example">
-			<div class="container layout-three">
-				<div class="row group-content">
-					<div class="col superscript">
-						<figure class="media-icon ">
-							<img alt="" class="img-fluid" src="http://local.frontendberg/wp-content/uploads/2019/01/icon_imovie_mac__el9krw4zjm82_large_2x.png">
-						</figure>
-						<h5>iMovie on MacBook Pro</h5>
-					</div>
-					<div class="col">
-						<h3>You have a great touch for making movies.</h3>
-					</div>
-					<div class="col">
-						<div>
-							<p>iMovie is even easier to use with the new MacBook Pro, featuring the revolutionary Touch Bar. The most useful commands automatically appear on the keyboard, right where you need them. And MacBook Pro easily powers through demanding 4K video projects so you can edit and export in record time.</p>
-							<a href="#" class="">Layout One</a>
-						</div>
-					</div>
-				</div>
-				<div class="row group-media">
-					<div class="col">
-						<figure class="media-image device-ipad portrait placeholder">
-							<img alt="" class="img-fluid" src="https://www.placehold.it/660x490">
-						</figure>
-					</div>
-				</div>
-			</div>
-			<div class="container layout-three">
-				<div class="row group-content">
-					<div class="col superscript">
-						<figure class="media-icon ">
-							<img alt="" class="img-fluid" src="http://local.frontendberg/wp-content/uploads/2019/01/icon_imovie_mac__el9krw4zjm82_large_2x.png">
-						</figure>
-						<h5>iMovie on MacBook Pro</h5>
-					</div>
-					<div class="col">
-						<h3>You have a great touch for making movies.</h3>
-					</div>
-					<div class="col">
-						<div>
-							<p>iMovie is even easier to use with the new MacBook Pro, featuring the revolutionary Touch Bar. The most useful commands automatically appear on the keyboard, right where you need them. And MacBook Pro easily powers through demanding 4K video projects so you can edit and export in record time.</p>
-							<a href="#" class="">Layout One</a>
-						</div>
-					</div>
-				</div>
-				<div class="row group-media">
-					<div class="col">
-						<figure class="media-image device-ipad portrait placeholder">
-							<img alt="" class="img-fluid" src="https://www.placehold.it/490x660">
-						</figure>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
+	</section>
 </main>
 
 <?php
