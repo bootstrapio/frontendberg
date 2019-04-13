8542f220d63a815d62b10cacdaf08791ed8343ed
Joseph <josephcvan@gmail.com>
2019-04-13 09:14:31 -0400

    Frontendberg CSS Updated

--- app/index.php
+++ app/index.php
@@ -2,20 +2,91 @@
 
 <main>
 	<section>
-		<div class="container intro-one">
-			<div class="row group-content">
-				<div class="col superscript">
-					<figure class="media-icon placeholder">
+		<div class="container block-group">
+			<div class="row group-media">
+				<div class="col">
+					<figure class="media-image device-iphone placeholder">
 						<img alt="" class="img-fluid" src="">
 					</figure>
+				</div>
+			</div>
+		</div>
+		<div class="container block-group">
+			<div class="row group-media">
+				<div class="col">
+					<figure class="media-image device-iphone portrait placeholder">
+						<img alt="" class="img-fluid" src="">
+					</figure>
+				</div>
+			</div>
+			<div class="row group-media">
+				<div class="col">
+					<figure class="media-image device-iphone portrait placeholder">
+						<img alt="" class="img-fluid" src="">
+					</figure>
+				</div>
+			</div>
+		</div>
+
+		<div class="container block-group">
+			<div class="row group-content">
+				<div class="col">
 					<h5>Deserunt Mollit</h5>
 				</div>
 				<div class="col">
-					<h2>Culpa Qui Oficia</h2>
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam.</p>
+						<a href="#" class="">Ut enim ad minim</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="container block-group">
+			<div class="row group-content">
+				<div class="col">
+					<h5>Deserunt Mollit</h5>
 				</div>
 				<div class="col">
-					<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam.</p>
-					<a href="#" class="">Ut enim ad minim</a>
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam.</p>
+						<a href="#" class="">Ut enim ad minim</a>
+				</div>
+			</div>
+			<div class="row group-content">
+				<div class="col">
+					<h5>Deserunt Mollit</h5>
+				</div>
+				<div class="col">
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam.</p>
+						<a href="#" class="">Ut enim ad minim</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="container block-group">
+			<div class="row group-content">
+				<div class="col">
+					<h5>Deserunt Mollit</h5>
+				</div>
+				<div class="col">
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam.</p>
+						<a href="#" class="">Ut enim ad minim</a>
+				</div>
+			</div>
+			<div class="row group-content">
+				<div class="col">
+					<h5>Deserunt Mollit</h5>
+				</div>
+				<div class="col">
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam.</p>
+						<a href="#" class="">Ut enim ad minim</a>
+				</div>
+			</div>
+			<div class="row group-content">
+				<div class="col">
+					<h5>Deserunt Mollit</h5>
+				</div>
+				<div class="col">
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam.</p>
+						<a href="#" class="">Ut enim ad minim</a>
 				</div>
 			</div>
 		</div>
